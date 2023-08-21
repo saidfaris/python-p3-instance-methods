15423ec64b491f8a652d1975c19d5fb30e769c2a
sidee <said.issack@student.moringaschool.com>
2023-08-21 23:08:54 +0300

    completed assighnment

--- lib/person.py
+++ lib/person.py
@@ -5,3 +5,13 @@ class Person:
 
     #Instance method definition
     pass
+    def talk(self):
+        print('Hello World!')
+
+    def walk(self):
+        print('The person is walking.')    
+
+
+saidy = Person()
+saidy.talk()  
+saidy.walk()  
